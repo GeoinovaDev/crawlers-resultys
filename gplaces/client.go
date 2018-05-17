8949c219adef7e8b4064171dc7e61bda2bbc2d2a
manoelnecojr@gmail <necoengenheiro@gmail.com>
2018-05-17 14:20:12 -0300

    sync

--- gplaces/client.go
+++ gplaces/client.go
@@ -1,6 +1,7 @@
 package gplaces
 
 import (
+	"git.resultys.com.br/lib/lower/convert/encode"
 	"git.resultys.com.br/lib/lower/exec"
 	"git.resultys.com.br/lib/lower/net/request"
 	"git.resultys.com.br/lib/lower/str"
@@ -26,7 +27,8 @@ func New(IP string) *Client {
 // Search ...
 func (client *Client) Search(nome string, cidade string, cep string, language string) (company *gplaces.Company) {
 	exec.Trying(3, func() {
-		url := client.createURL(str.Format("/search?nome={0}&cidade={1}&cep={2}&language={3}", nome, cidade, cep, language))
+		url := str.Format("/search?nome={0}&cidade={1}&cep={2}&language={3}", encode.URL(nome), encode.URL(cidade), encode.URL(cep), encode.URL(language))
+		url = client.createURL(url)
 		protocol := protocol{}
 
 		err := request.New(url).GetJSON(&protocol)
